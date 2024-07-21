1e49b155bbb6a7643d4ed0d49260f2c39825bb19
Luca-Gerace <geraceluca91@gmail.com>
2024-07-21 20:01:34 +0200

    mern-blog: minor fixes login & register page on FE

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { userLogin } from "../services/api";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { userLogin } from '../services/api';
 
 export default function Login() {
 
     // Hook - form data
     const [formData, setFormData] = useState({
-        email: "",
-        password: "",
+        email: '',
+        password: '',
     });
 
     // Hook - navigation
@@ -26,39 +26,39 @@ export default function Login() {
             const response = await userLogin(formData);
 
             // store auth token inside localstorage
-            localStorage.setItem("token", response.token);
+            localStorage.setItem('token', response.token);
 
             // Trigger l'evento storage per aggiornare la Navbar
-            window.dispatchEvent(new Event("storage"));
+            window.dispatchEvent(new Event('storage'));
 
             // Go to home
-            navigate("/");
+            navigate('/');
 
         } catch (error) {
-            console.error("Login error:", error);
-            alert("Login error. Retry.");
+            console.error('Login error:', error);
+            alert('Login error. Retry.');
         }
     };
 
     return (
-        <div className="container">
+        <div className='container'>
             <h2>Login</h2>
             <form onSubmit={handleSubmit}>
                 <input
-                type="email"
-                name="email"
-                placeholder="Email"
+                type='email'
+                name='email'
+                placeholder='Email'
                 onChange={handleChange}
                 required
                 />
                 <input
-                type="password"
-                name="password"
-                placeholder="Password"
+                type='password'
+                name='password'
+                placeholder='Password'
                 onChange={handleChange}
                 required
                 />
-                <button type="submit">Accedi</button>
+                <button type='submit'>Accedi</button>
             </form>
         </div>
     );
